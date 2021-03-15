19b028d5fd21481489ef5193efac38fe8090e71a
Jules Marécaille <jules.marecaille@gmail.com>
2021-03-15 15:06:54 +0100

    add drag n drop to pieces

--- front_end/src/chess/ui/Square.js
+++ front_end/src/chess/ui/Square.js
@@ -10,6 +10,16 @@ class Square extends React.Component {
         this.props.onClick(this.props.square);
     }
 
+    handleDragStart(evt){
+        this.props.dragStart(evt, this.props.square);
+    }
+
+    handleDragOver(evt){
+        evt.preventDefault();
+        evt.dataTransfer.dropEffect = "move";
+        this.props.dragOver(this.props.square);
+    }
+
     render() {
         let option_marker = '';
         let piece = '';
@@ -20,7 +30,11 @@ class Square extends React.Component {
         }
         // Add Piece if there's a piece on this square
         if (this.props.piece) {
-            piece = <Piece piece={this.props.piece}/>
+            piece = <Piece piece={this.props.piece}
+                           dragStart={this.handleDragStart.bind(this)}
+                           dragEnd={this.props.dragEnd}
+                           dragging={this.props.dragging}
+                           isDraggable={true}/>
         }
         //Add check marker if this piece is in check
         if (this.props.isCheck){
@@ -30,7 +44,8 @@ class Square extends React.Component {
         return (
         <React.Fragment>
             <td className={`${this.props.color} ${this.props.isSelected ? "selected" : ""} ${this.props.isClickable ? "clickable" : ""}`}
-                onClick={this.handleClick.bind(this)}>
+                onClick={this.handleClick.bind(this)}
+                onDragOver={this.handleDragOver.bind(this)}>
                 {option_marker}
                 {piece}
                 {check_marker}
